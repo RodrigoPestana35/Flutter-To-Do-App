b6b534b5ef93146cf90d4f9908fcb5b31078a5db
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-09-25 00:49:23 +0100

    Refactor DialogBox widget to include a styled AlertDialog with a text input field for adding new tasks

--- lib/util/dialog_box.dart
+++ lib/util/dialog_box.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -7,6 +7,21 @@ class DialogBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog();
+    return AlertDialog(
+      backgroundColor: Colors.blueGrey[300],
+      content: Container(
+        height: 120,
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Add new task",
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
