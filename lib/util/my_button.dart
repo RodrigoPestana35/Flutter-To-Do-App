b8e65ebbd7dfd4c1bbfe23ff4f311eebb197cff6
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-09-25 00:52:59 +0100

    Refactor MyButton widget to accept text and onPressed callback

--- lib/util/my_button.dart
+++ lib/util/my_button.dart
@@ -1,10 +1,22 @@
+// ignore_for_file: sort_child_properties_last, must_be_immutable
+
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
-  const MyButton({super.key});
+  final String text;
+  VoidCallback onPressed;
+  MyButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialButton(
+      onPressed: onPressed,
+      child: Text(text),
+      color: Theme.of(context).primaryColor,
+    );
   }
 }
