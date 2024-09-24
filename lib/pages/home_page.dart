3d9fc9df44860de30b097296f251619ebf0d0e3b
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-09-25 00:40:57 +0100

    add todoTiles builder and style todoTile

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:todoapp/util/todo_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,6 +11,18 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //list of todo tasks
+  List toDoList = [
+    ["Make something", false],
+    ["Make other thing", false],
+  ];
+
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,10 +31,15 @@ class _HomePageState extends State<HomePage> {
         title: Text("To Do App"),
         elevation: 0,
       ),
-      body: ListView(
-        children: [
-          //ToDoList(),
-        ],
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return TodoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(value, index),
+          );
+        },
       ),
     );
   }
--- lib/util/todo_tile.dart
+++ lib/util/todo_tile.dart
@@ -1,10 +1,45 @@
+// ignore_for_file: sort_child_properties_last, prefer_const_constructors, prefer_const_literals_to_create_immutables, must_be_immutable
+
 import 'package:flutter/material.dart';
 
 class TodoTile extends StatelessWidget {
-  const TodoTile({super.key});
+  final String taskName;
+  final bool taskCompleted;
+  Function(bool?)? onChanged;
+
+  TodoTile({
+    super.key,
+    required this.taskName,
+    required this.taskCompleted,
+    required this.onChanged,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.only(left: 25, right: 25, top: 25),
+      child: Container(
+        padding: EdgeInsets.all(24),
+        child: Row(children: [
+          Checkbox(
+            value: taskCompleted,
+            onChanged: onChanged,
+            activeColor: Colors.green[800],
+          ),
+          Text(
+            taskName,
+            style: TextStyle(
+                fontSize: 18,
+                decoration: taskCompleted
+                    ? TextDecoration.lineThrough
+                    : TextDecoration.none),
+          ),
+        ]),
+        decoration: BoxDecoration(
+          color: Colors.blueGrey[100],
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+    );
   }
 }
