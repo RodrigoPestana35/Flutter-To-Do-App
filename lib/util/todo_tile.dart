34e0bf6244f1c32175768fcf4ce83c08063f2e8b
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-09-25 01:25:05 +0100

    add delete task and refractor UI

--- lib/util/todo_tile.dart
+++ lib/util/todo_tile.dart
@@ -1,43 +1,56 @@
 // ignore_for_file: sort_child_properties_last, prefer_const_constructors, prefer_const_literals_to_create_immutables, must_be_immutable
 
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class TodoTile extends StatelessWidget {
   final String taskName;
   final bool taskCompleted;
   Function(bool?)? onChanged;
+  Function(BuildContext)? deleteTask;
 
   TodoTile({
     super.key,
     required this.taskName,
     required this.taskCompleted,
     required this.onChanged,
+    required this.deleteTask,
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 25, right: 25, top: 25),
-      child: Container(
-        padding: EdgeInsets.all(24),
-        child: Row(children: [
-          Checkbox(
-            value: taskCompleted,
-            onChanged: onChanged,
-            activeColor: Colors.green[800],
-          ),
-          Text(
-            taskName,
-            style: TextStyle(
-                fontSize: 18,
-                decoration: taskCompleted
-                    ? TextDecoration.lineThrough
-                    : TextDecoration.none),
+      child: Slidable(
+        endActionPane: ActionPane(motion: StretchMotion(), children: [
+          SlidableAction(
+            onPressed: deleteTask,
+            icon: Icons.delete,
+            backgroundColor: Colors.red.shade300,
+            borderRadius: BorderRadius.circular(12),
           ),
         ]),
-        decoration: BoxDecoration(
-          color: Colors.blueGrey[100],
-          borderRadius: BorderRadius.circular(12),
+        child: Container(
+          padding: EdgeInsets.all(24),
+          child: Row(children: [
+            Checkbox(
+              value: taskCompleted,
+              onChanged: onChanged,
+              activeColor: Colors.green[800],
+            ),
+            Text(
+              taskName,
+              style: TextStyle(
+                  fontSize: 18,
+                  decoration: taskCompleted
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none),
+            ),
+          ]),
+          decoration: BoxDecoration(
+            color: Colors.blueGrey[100],
+            borderRadius: BorderRadius.circular(12),
+          ),
         ),
       ),
     );
